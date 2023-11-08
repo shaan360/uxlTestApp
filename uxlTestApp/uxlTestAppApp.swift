63c5c6d2e306aad369af4cf0bf598ace185195fe
shaan360 <shaan@uexel.com>
2023-11-08 11:01:35 +0500

    Initial Commit

---
+++ uxlTestApp/uxlTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  uxlTestAppApp.swift
+//  uxlTestApp
+//
+//  Created by Shaan on 08/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct uxlTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
